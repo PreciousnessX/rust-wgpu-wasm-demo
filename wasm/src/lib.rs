10fbee5f1e016e49dac6af259f26a34ce0aa85f6
Leon <xiaohongbo@yushu.biz>
2023-09-15 08:38:01 +0800

    feat:初始化项目

---
+++ wasm/src/lib.rs
@@ -0,0 +1,19 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &JsValue);
+}
+#[wasm_bindgen]
+pub fn greet(s: &str) {
+    let message = format!("你好 {}", s);
+    alert(&JsValue::from_str(&message));
+}
