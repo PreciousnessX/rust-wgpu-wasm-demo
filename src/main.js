dc6fb67c650e75aed89cc1ba8916af9a9d4e6f0e
Leon <xiaohongbo@yushu.biz>
2023-09-28 09:48:07 +0800

    feat: bevy 打包成wasm

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-import * as wasm from '../wasm/pkg/rust_wasm_demo';
+// import * as wasm from '../wasm/pkg/rust_wasm_demo';
 
-wasm.greet('vit-app');
+// wasm.greet('vit-app');
 
+import * as wasm from '../bevy-web/pkg/bevy_web'
+
+wasm.run();
 
 
 createApp(App).mount('#app')
